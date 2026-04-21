6eba3df3adca69b3db80d227b4959786b23b5117
LSH005 <isihyeon005shingu@gmail.com>
2026-04-21 18:21:04 +0900

    [test] 레이저 발사체 구현

---
+++ Assets/Scripts/Test/LazerShooter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LazerShooter : MonoBehaviour
+{
+    [Header("발사 주기")]
+    public float shootInterval = 3f;
+
+    [Header("레이저 설정")]
+    public EnemyLaser laserPrefab;
+    public Transform target;
+
+    private void Start()
+    {
+        StartCoroutine(laserFire());
+    }
+
+    IEnumerator laserFire()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(shootInterval);
+            EnemyLaser laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            laser.SetTarget(target);
+            laser.SetOrigin(transform);
+        }
+    }
+}
